ab5ce1f3ada4035b1d7cccae257656f42e0438c7
Jaeyeop-Jung <jjy5349@naver.com>
2022-04-15 18:55:41 +0900

    init commit

---
+++ src/main/java/com/capstone/hyperledgerfabrictransferserver/HyperledgerFabricTransferServerApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.hyperledgerfabrictransferserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperledgerFabricTransferServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyperledgerFabricTransferServerApplication.class, args);
+	}
+
+}
