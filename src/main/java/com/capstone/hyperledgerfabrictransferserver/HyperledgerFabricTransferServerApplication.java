66152b58228958effa15814675eb939ed520d824
Jaeyeop-Jung <jjy5349@naver.com>
2022-05-03 10:59:16 +0900

    feature: EnrollAdmin, RegisterUser

--- src/main/java/com/capstone/hyperledgerfabrictransferserver/HyperledgerFabricTransferServerApplication.java
+++ src/main/java/com/capstone/hyperledgerfabrictransferserver/HyperledgerFabricTransferServerApplication.java
@@ -1,13 +1,17 @@
 package com.capstone.hyperledgerfabrictransferserver;
 
+import com.capstone.hyperledgerfabrictransferserver.util.EnrollAdmin;
+import com.capstone.hyperledgerfabrictransferserver.util.RegisterUser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HyperledgerFabricTransferServerApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(HyperledgerFabricTransferServerApplication.class, args);
+		EnrollAdmin.main(null);
+		RegisterUser.main(null);
 	}
 
 }
