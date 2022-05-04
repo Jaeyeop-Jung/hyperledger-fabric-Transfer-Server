94f1ef5cba6fe46f619598cfbeb43a53af6f1ffb
Jaeyeop-Jung <jjy5349@naver.com>
2022-05-04 17:17:29 +0900

    feature: JwtTokenProvider 생성

--- src/main/java/com/capstone/hyperledgerfabrictransferserver/filter/JwtTokenProvider.java
+++ src/main/java/com/capstone/hyperledgerfabrictransferserver/filter/JwtTokenProvider.java
@@ -1,4 +1,93 @@
 package com.capstone.hyperledgerfabrictransferserver.filter;
 
+import com.capstone.hyperledgerfabrictransferserver.domain.User;
+import io.jsonwebtoken.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.core.config.plugins.validation.constraints.Required;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
 public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    private final UserDetailsService userDetailsService;
+
+    private final Long ACCESS_TOKEN_EXPIRE_TIME = 60 * 60 * 1000L;
+
+    /**
+     * methodName : generateJwtToken
+     * author : Jaeyeop Jung
+     * description : User 도메인 객체를 이용해 JWT 토큰 발행
+     *
+     * @param user the user
+     * @return the jwt token
+     */
+    public String generateJwtToken(User user){
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setHeaderParam("typ", "ACCESS_TOKEN")
+                .setHeaderParam("alg", "HS256")
+                .setSubject(user.getId().toString())
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + ACCESS_TOKEN_EXPIRE_TIME))
+                .claim("role", user.getUserRole().toString())
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    /**
+     * methodName : findUserIdByJwt
+     * author : Jaeyeop Jung
+     * description : JWT토큰으로 User 도메인 ID를 찾음
+     *
+     * @param token the token
+     * @return the user id by jwt
+     */
+    public Long findUserIdByJwt(String token){
+        Claims claims = Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody();
+        Long userId = Long.valueOf(claims.getSubject());
+
+        return userId;
+    }
+
+    /**
+     * methodName : validateToken
+     * author : Jaeyeop Jung
+     * description : JWT 토큰을 검증
+     *
+     * @param token the token
+     * @return the token
+     */
+    public boolean validateToken(String token){
+        try {
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return true;
+        } catch (SignatureException ex) {
+            log.error("Invalid JWT signature");
+        } catch (MalformedJwtException ex) {
+            log.error("Invalid JWT token");
+        } catch (ExpiredJwtException ex) {
+            log.error("Expired JWT token");
+        } catch (UnsupportedJwtException ex) {
+            log.error("Unsupported JWT token");
+        } catch (IllegalArgumentException ex) {
+            log.error("JWT claims string is empty.");
+        } catch (NullPointerException ex){
+            log.error("JWT RefreshToken is empty");
+        }
+        return false;
+    }
 }
