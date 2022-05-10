46ea1ab9c0bae2ced9ec52c66bb300b39d4bb9e1
Jaeyeop-Jung <jjy5349@naver.com>
2022-05-10 16:40:30 +0900

    feature: UserApiControllerImplTest 생성

---
+++ src/test/java/com/capstone/hyperledgerfabrictransferserver/api/UserApiControllerImplTest.java
@@ -0,0 +1,20 @@
+package com.capstone.hyperledgerfabrictransferserver.api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserApiControllerImplTest {
+
+    @Test
+    void join() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void changePassword() {
+    }
+}
