e39f90e6100bcc6b1cef02a692cc2d055dc2ffb6
Jaeyeop-Jung <jjy5349@naver.com>
2022-06-28 13:08:15 +0900

    feature: AdminServiceImpl 생성

---
+++ src/main/java/com/capstone/hyperledgerfabrictransferserver/api/AdminApiControllerImpl.java
@@ -0,0 +1,27 @@
+package com.capstone.hyperledgerfabrictransferserver.api;
+
+import com.capstone.hyperledgerfabrictransferserver.dto.UserLoginResponse;
+import com.capstone.hyperledgerfabrictransferserver.dto.UserLoginRequest;
+import com.capstone.hyperledgerfabrictransferserver.service.AdminServiceImpl;
+import com.capstone.hyperledgerfabrictransferserver.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin")
+public class AdminApiControllerImpl {
+
+    private final AdminServiceImpl adminService;
+
+    @GetMapping("/login")
+    public ResponseEntity<UserLoginResponse> login(
+        @ModelAttribute UserLoginRequest userLoginRequest
+    ){
+        return ResponseEntity.ok(adminService.login(userLoginRequest));
+    }
+}
