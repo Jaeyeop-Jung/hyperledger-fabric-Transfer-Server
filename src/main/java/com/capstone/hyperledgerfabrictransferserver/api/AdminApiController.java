b95365e0e0f24559defa5b7059f65fda82d238e1
Jaeyeop-Jung <jjy5349@naver.com>
2022-07-04 14:52:12 +0900

    feature: AdminApiControllerImpl 코인기능 추가

---
+++ src/main/java/com/capstone/hyperledgerfabrictransferserver/api/AdminApiController.java
@@ -0,0 +1,16 @@
+package com.capstone.hyperledgerfabrictransferserver.api;
+
+import com.capstone.hyperledgerfabrictransferserver.dto.CoinCreateRequest;
+import com.capstone.hyperledgerfabrictransferserver.dto.CoinModifyRequest;
+import com.capstone.hyperledgerfabrictransferserver.dto.UserLoginRequest;
+import com.capstone.hyperledgerfabrictransferserver.dto.UserLoginResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+public interface AdminApiController {
+
+    public ResponseEntity<UserLoginResponse> login(UserLoginRequest userLoginRequest);
+    public ResponseEntity<Void> createCoin(CoinCreateRequest coinCreateRequest);
+
+    public ResponseEntity<Void> deleteCoin(CoinModifyRequest coinModifyRequest);
+}
--- src/main/java/com/capstone/hyperledgerfabrictransferserver/api/AdminApiControllerImpl.java
+++ src/main/java/com/capstone/hyperledgerfabrictransferserver/api/AdminApiControllerImpl.java
@@ -1,21 +1,22 @@
 package com.capstone.hyperledgerfabrictransferserver.api;
 
+import com.capstone.hyperledgerfabrictransferserver.dto.CoinCreateRequest;
+import com.capstone.hyperledgerfabrictransferserver.dto.CoinModifyRequest;
 import com.capstone.hyperledgerfabrictransferserver.dto.UserLoginResponse;
 import com.capstone.hyperledgerfabrictransferserver.dto.UserLoginRequest;
+import com.capstone.hyperledgerfabrictransferserver.service.CoinService;
 import com.capstone.hyperledgerfabrictransferserver.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/admin")
-public class AdminApiControllerImpl {
+public class AdminApiControllerImpl implements AdminApiController{
 
     private final UserService userService;
+    private final CoinService coinService;
 
     @GetMapping("/login")
     public ResponseEntity<UserLoginResponse> login(
@@ -23,4 +24,22 @@ public class AdminApiControllerImpl {
     ){
         return ResponseEntity.ok(userService.login(userLoginRequest));
     }
+
+    @Override
+    @PostMapping("/coin")
+    public ResponseEntity<Void> createCoin(@RequestBody CoinCreateRequest coinCreateRequest) {
+
+        coinService.create(coinCreateRequest);
+
+        return ResponseEntity.ok(null);
+    }
+
+    @Override
+    @DeleteMapping("/coin")
+    public ResponseEntity<Void> deleteCoin(@RequestBody CoinModifyRequest coinModifyRequest) {
+        coinService.delete(coinModifyRequest);
+
+        return ResponseEntity.ok(null);
+    }
+
 }
