7cf61dbd5a4605a4fef52e750f7b17ee4b4f3ee1
Jaeyeop-Jung <jjy5349@naver.com>
2022-07-09 15:54:14 +0900

    feature: Transaction 조회 AdminApiController 추가

--- src/main/java/com/capstone/hyperledgerfabrictransferserver/api/AdminApiController.java
+++ src/main/java/com/capstone/hyperledgerfabrictransferserver/api/AdminApiController.java
@@ -1,9 +1,12 @@
 package com.capstone.hyperledgerfabrictransferserver.api;
 
 import com.capstone.hyperledgerfabrictransferserver.dto.*;
+import io.swagger.models.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface AdminApiController {
@@ -11,4 +14,6 @@ public interface AdminApiController {
     public ResponseEntity<UserLoginResponse> login(UserLoginRequest userLoginRequest);
 
     public ResponseEntity<PagingUserDto> getAllUser(int page);
+
+    public ResponseEntity<PagingTransferResponseDto> getAllTradeBy(int page, Long sender, Long receiver, LocalDateTime dateCreated);
 }
--- src/main/java/com/capstone/hyperledgerfabrictransferserver/api/AdminApiControllerImpl.java
+++ src/main/java/com/capstone/hyperledgerfabrictransferserver/api/AdminApiControllerImpl.java
@@ -1,10 +1,14 @@
 package com.capstone.hyperledgerfabrictransferserver.api;
 
 import com.capstone.hyperledgerfabrictransferserver.dto.*;
+import com.capstone.hyperledgerfabrictransferserver.service.TradeService;
 import com.capstone.hyperledgerfabrictransferserver.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 
 @RestController
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 public class AdminApiControllerImpl implements AdminApiController{
 
     private final UserService userService;
+    private final TradeService tradeService;
 
     @GetMapping("/login")
     public ResponseEntity<UserLoginResponse> login(
@@ -27,5 +32,15 @@ public class AdminApiControllerImpl implements AdminApiController{
             @RequestParam(required = false, defaultValue = "1") int page
     ) {
         return ResponseEntity.ok(userService.getAllUser(page));
+    }
+
+    @Override
+    public ResponseEntity<PagingTransferResponseDto> getAllTradeBy(
+            int page,
+            Long sender,
+            Long receiver,
+            LocalDateTime dateCreated
+    ) {
+        return ResponseEntity.ok(tradeService.getAllTradeBy(page, sender, receiver, dateCreated));
     }
 }
