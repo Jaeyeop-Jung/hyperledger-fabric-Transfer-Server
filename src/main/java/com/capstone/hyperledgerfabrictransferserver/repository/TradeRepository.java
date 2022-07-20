3f8dac1e77c196618df3965964a788841beb83e3
Jaeyeop-Jung <jjy5349@naver.com>
2022-07-20 14:15:52 +0900

    feature: repository findByTransactionId 추가

--- src/main/java/com/capstone/hyperledgerfabrictransferserver/repository/TradeRepository.java
+++ src/main/java/com/capstone/hyperledgerfabrictransferserver/repository/TradeRepository.java
@@ -2,12 +2,15 @@ package com.capstone.hyperledgerfabrictransferserver.repository;
 
 import com.capstone.hyperledgerfabrictransferserver.domain.User;
 import com.capstone.hyperledgerfabrictransferserver.domain.Trade;
+import com.capstone.hyperledgerfabrictransferserver.dto.UserDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 public interface TradeRepository extends JpaRepository<Trade, Long> {
 
@@ -24,5 +27,8 @@ public interface TradeRepository extends JpaRepository<Trade, Long> {
              Long receiverStudentId,
              Pageable pageable
     );
+
+    Optional<Trade> findByTransactionId(String transactionId);
+
 
 }
