506d95530079fdbd7c34fc89feeb82268f9a96a7
Jaeyeop-Jung <jjy5349@naver.com>
2022-07-20 23:12:58 +0900

    refactor: table schema 재정의

--- sql/CREATE_TABLE.sql
+++ sql/CREATE_TABLE.sql
@@ -1,6 +1,9 @@
-DROP TABLE IF EXISTS shopimage ;
+DROP TABLE IF EXISTS storeimage ;
 DROP TABLE IF EXISTS trade ;
-DROP TABLE IF EXISTS shop ;
+DROP TABLE IF EXISTS store ;
+DROP TABLE IF EXISTS STUDENT;
+DROP TABLE IF EXISTS STOREMANGER;
+DROP TABLE IF EXISTS ADMIN;
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS coin;
 
@@ -15,24 +18,45 @@ CREATE TABLE coin(
 
 CREATE TABLE user(
                      user_id BIGINT NOT NULL AUTO_INCREMENT,
-                     student_id BIGINT NOT NULL,
                      password VARCHAR(255) NOT NULL,
                      role VARCHAR(255) NOT NULL,
                      name VARCHAR(255) NOT NULL,
                      date_created datetime not null,
                      last_updated datetime not null,
-                     PRIMARY KEY(user_id),
-                     UNIQUE KEY(student_id)
+                     PRIMARY KEY(user_id)
 );
 
-CREATE TABLE shop(
-                     shop_id BIGINT NOT NULL AUTO_INCREMENT ,
+CREATE TABLE STUDENT(
+                    user_id BIGINT NOT NULL ,
+                    student_number VARCHAR(255) NOT NULL,
+                    primary key (user_id),
+                    foreign key (user_id) references user(user_id)
+
+);
+
+CREATE TABLE STOREMANGER(
+                        user_id BIGINT NOT NULL ,
+                        PHONE_NUMBER VARCHAR(255) NOT NULL,
+                        primary key (user_id),
+                        foreign key (user_id) references user(user_id)
+
+);
+
+CREATE TABLE ADMIN(
+                        user_id BIGINT NOT NULL ,
+                        primary key (user_id),
+                        foreign key (user_id) references user(user_id)
+
+);
+
+CREATE TABLE store(
+                     store_id BIGINT NOT NULL AUTO_INCREMENT ,
                      name VARCHAR(255) NOT NULL,
                      phone_number VARCHAR(255) NOT NULL,
                      address VARCHAR(255) NOT NULL,
                      date_created datetime not null,
                      last_updated datetime not null,
-                     PRIMARY KEY(shop_id)
+                     PRIMARY KEY(store_id)
 );
 
 CREATE TABLE trade(
@@ -53,14 +77,14 @@ CREATE TABLE trade(
 );
 
 
-CREATE TABLE shopimage(
-                          shopimage_id BIGINT NOT NULL AUTO_INCREMENT ,
+CREATE TABLE storeimage(
+                          storeimage_id BIGINT NOT NULL AUTO_INCREMENT ,
                           shop_id BIGINT NOT NULL,
                           name VARCHAR(255) NOT NULL,
                           size BIGINT NOT NULL,
                           extension VARCHAR(255) NOT NULL,
                           date_created datetime not null,
                           last_updated datetime not null,
-                          PRIMARY KEY(SHOPIMAGE_ID),
-                          FOREIGN KEY(shop_id) REFERENCES shop(shop_id)
+                          PRIMARY KEY(storeimage_id),
+                          FOREIGN KEY(storeimage_id) REFERENCES store(store_id)
 );
