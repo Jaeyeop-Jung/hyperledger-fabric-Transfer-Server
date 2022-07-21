6a5ca41e94e0665f2b6885a94d858ee0b4d1e60c
Jaeyeop-Jung <jjy5349@naver.com>
2022-07-21 14:48:37 +0900

    refactor: User 엔티티 수정

--- src/main/java/com/capstone/hyperledgerfabrictransferserver/domain/User.java
+++ src/main/java/com/capstone/hyperledgerfabrictransferserver/domain/User.java
@@ -19,10 +19,6 @@ public class User extends BaseEntity{
     private Long id;
 
     @NotNull
-    @Column(name = "STUDENT_ID")
-    private Long studentId;
-
-    @NotNull
     private String password;
 
     @NotNull
@@ -32,17 +28,6 @@ public class User extends BaseEntity{
 
     @NotNull
     private String name;
-
-    private User(Long studentId, String password, UserRole userRole, String name) {
-        this.studentId = studentId;
-        this.password = password;
-        this.userRole = userRole;
-        this.name = name;
-    }
-
-    public static User of(Long studentId, String password, UserRole userRole, String name){
-        return new User(studentId, password, userRole, name);
-    }
 
     public void changePassword(String password){
         this.password = password;
