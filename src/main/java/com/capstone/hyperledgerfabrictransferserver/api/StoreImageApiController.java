6cd0a37ba2755be0bcab2cf82570fc2169a2651d
Jaeyeop-Jung <jjy5349@naver.com>
2022-07-25 22:15:47 +0900

    feature: StoreImage backbone

---
+++ src/main/java/com/capstone/hyperledgerfabrictransferserver/api/StoreImageApiController.java
@@ -0,0 +1,11 @@
+package com.capstone.hyperledgerfabrictransferserver.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/store")
+public class StoreImageApiController {
+}
---
+++ src/main/java/com/capstone/hyperledgerfabrictransferserver/repository/StoreImageRepository.java
@@ -0,0 +1,7 @@
+package com.capstone.hyperledgerfabrictransferserver.repository;
+
+import com.capstone.hyperledgerfabrictransferserver.domain.StoreImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreImageRepository extends JpaRepository<StoreImage, Long> {
+}
